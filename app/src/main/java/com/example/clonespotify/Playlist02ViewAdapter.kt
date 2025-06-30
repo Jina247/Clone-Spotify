1db449131dedc63cb147911ae45f0495fbf60bf1
Jina_atLibrary <trannnhii2407@gmail.com>
2025-06-30 17:27:24 +0800

    Finish RecyclerViewAdapter

---
+++ app/src/main/java/com/example/clonespotify/Playlist02ViewAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.clonespotify
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
+
+class Playlist02ViewAdapter(private val item : List<Playlist02>) :
+    RecyclerView.Adapter<Playlist02ViewAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val image : ImageButton = view.findViewById(R.id.AlbImage)
+        val description : Button = view.findViewById(R.id.Playlist02Tittle)
+    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_playlist02, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.image.setImageResource(item[position].albumCover)
+        holder.description.text = item[position].description
+    }
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
+
+}
