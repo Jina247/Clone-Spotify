df92ad6b74f93e24600910dd45883db671a92892
jina <trannnhii2407@gmail.com>
2025-06-30 14:06:51 +0800

    started off creating login screen and home screen

---
+++ app/src/main/java/com/example/clonespotify/MainScreen.kt
@@ -0,0 +1,45 @@
+package com.example.clonespotify
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val playlist01 : List<Playlist> = listOf(
+            Playlist(R.drawable.alec_ben_ab, getString(R.string.narrated_for_you)),
+            Playlist(R.drawable.alec_ben_ab2, getString(R.string.let_me_down_slowly)),
+            Playlist(R.drawable.taylor_swift_album, getString(R.string.lover)),
+        )
+
+        val recyclerView01 = findViewById<RecyclerView>(R.id.PlaylistRecycler01)
+        recyclerView01.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView01.adapter = PlaylistViewAdapter(playlist01)
+
+        val playlist02 : List<Playlist> = listOf(
+            Playlist(R.drawable.ari_album, getString(R.string.eternal_sunshine_deluxe_brighter_days_ahead)),
+            Playlist(R.drawable.shawn_album, "Shawn Mendes (Deluxe)"),
+            Playlist(R.drawable.rosie_ab, "APT.")
+        )
+
+        val recyclerView02 = findViewById<RecyclerView>(R.id.PlaylistRecycler02)
+        recyclerView02.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView02.adapter = PlaylistViewAdapter(playlist02)
+    }
+
+
+
+}
